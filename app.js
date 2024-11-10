9031e4e69763582df5ff9e332463941bc6f19d50
Arek <areksonic@gmail.com>
2024-11-10 22:00:50 +0100

    IT WORKS

--- app.js
+++ app.js
@@ -1,14 +1,73 @@
 let gameGrid = [[0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]];
-let tempAssignedColors = [];
 let heldBlockValue = 0;
+let scoreValue = 0;
 let tableSetHolder;
-let blocks = [/*Square 3x3*/"0000001110011100111000000", /*T*/ "0000001110001000000000000"];
+let blocks = [
+    // Square block - 3x3 in the center of 5x5 grid (only one orientation)
+    "0000001110011100111000000",
+
+    // T-shaped block - 4 orientations
+    "0000000000001000111000000", // Default orientation
+    "0000001000011000100000000", // 90 degrees
+    "0000001110001000000000000", // 180 degrees
+    "0000000010001100001000000", // 270 degrees
+
+    // L-shaped block - 4 orientations
+    "0000000100001000011000000", // Default orientation
+    "0000000000011100100000000", // 90 degrees
+    "0000000110001000010000000", // 180 degrees
+    "0000000010011100000000000", // 270 degrees
+
+    // Big L-shaped block
+    "0000001000010000111000000", // Default orientation
+    "0000001110010000100000000", // 90 degrees
+    "0000001110000100001000000", // 180 degrees
+    "0000000010000100111000000", // 270 degrees
+
+    // Small L-shaped block
+    "0000000100011000000000000", // Default orientation
+    "0000000100001100000000000", // 90 degrees
+    "0000000000001100010000000", // 180 degrees
+    "0000000000011000010000000", // 270 degrees
+
+    // Rectangle 2x3
+    "0000000000011100111000000", // Horizontal
+    "0000001100011000110000000", // Vertical
+
+    // Line block l4 - 2 orientations
+    "0010000100001000010000000", // Vertical
+    "0000000000011110000000000", // Horizontal
+
+    // Line block l5 - 2 orientations
+    "0010000100001000010000100", // Vertical
+    "0000000000111110000000000", // Horizontal
+
+    // Z-shaped block - 2 orientations
+    "0000000100001100001000000", // Default orientation
+    "0000000000011000011000000", // 90 degrees
+
+    // 2 corners - 2 orientations
+    "0000001000001000000000000",
+    "0000000010001000000000000",
+
+    // Mirror images of L-shaped block - 4 orientations
+    "0000000100001000110000000", // Mirrored Default orientation
+    "0000001000011100000000000", // Mirrored 90 degrees
+    "0000000110001000010000000", // Mirrored 180 degrees
+    "0000000000011100001000000", // Mirrored 270 degrees
+
+    // Mirror images of Z-shaped block - 2 orientations
+    "0000001000011000010000000", // Mirrored Default orientation
+    "0000000000001100110000000"  // Mirrored 90 degrees
+
+];
 let colors = ["yellow", "red", "blue", "green", "cyan", "purple", "orange"];
 
 let gameArea = document.getElementById("gameArea");
 let tableCounter = 0;
+let setTableCounter = 0;
 
-function tableGen(tableWidth, tableHeight, tableWidth_px, tableHeight_px, tableId, trId, tdId, borderStyle, isMainArea){
+function tableGen(tableWidth, tableHeight, tableWidth_px, tableHeight_px, tableId, trId, tdId, borderStyle, td_borderStyle){
     let gameTable = document.createElement("table");
 
     gameTable.id = String(tableId) + tableCounter;
@@ -35,7 +94,7 @@ function tableGen(tableWidth, tableHeight, tableWidth_px, tableHeight_px, tableI
             let gameTd = document.createElement("td");
 
             gameTd.id = String(tdId) + "_" + tableCounter + "_" + tdCounterX + "_" + tdCounterY;
-            gameTd.style.border = "1px solid black";
+            gameTd.style.border = td_borderStyle;
             gameTd.style.backgroundColor = "white";
 
             gameTr.appendChild(gameTd);
@@ -52,9 +111,11 @@ function tableGen(tableWidth, tableHeight, tableWidth_px, tableHeight_px, tableI
 }
 
 function setGen(){
+    tempAssignedColors = [];
+
     // Select correct tableSet area
-    for(let x = 0; x<(tableCounter-1); x++){
-        console.log("REPETITION " + x);
+    for(let x = 0; x<3; x++){
+        console.log("REPETITION " + (x + (tableCounter-1)));
         let tableSet = document.getElementById(("tableSet"+(x+1)));
         let tdCounterX = 0;
         let tdCounterY = 0;
@@ -66,11 +127,11 @@ function setGen(){
         // Repeat 25 times for each pixel
         for(let y = 0; y<25; y++){
 
-            let tdSet = document.getElementById(("tdSet" + "_" + (x+1) + "_" + tdCounterX + "_" + tdCounterY));
-            console.log(("tdSet" + "_" + (x+1) + "_" + tdCounterX + "_" + tdCounterY));
+            let tdSet = document.getElementById(`tdSet_${setTableCounter+1}_${tdCounterX}_${tdCounterY}`);
+            console.log(`tdSet_${setTableCounter+1}_${tdCounterX}_${tdCounterY}`);
 
             // If it is meant to be filled, color it with current color
-            if(tempBlock[y] == 1){
+            if(tempBlock[y] == "1"){
                 tdSet.style.backgroundColor = tempColor;
                 // Storing colors till placement
                 tempAssignedColors[x] = tempColor;
@@ -88,32 +149,87 @@ function setGen(){
                 tdCounterY++;
             }
         }
+        setTableCounter++;
     }
     placeElement();
+}
+
+
+function moveBlock(ev){
+    if(tableSetHolder){
+        tableSetHolder.style.transform = 'translateY(' + (ev.clientY - 200) + 'px)';
+        tableSetHolder.style.transform += 'translateX(' + (ev.clientX - 105) + 'px)';        
+    }
 }
 
 function selectElement(tempBlock, x){
     if(heldBlockValue==0){
         heldBlockValue = tempBlock;
-        tableSetHolder = document.getElementById('tableSet' + (x+1));
+        tableSetHolder = document.getElementById('tableSet' + (x + (tableCounter-3)));
         tableSetHolder.style.position = 'absolute';
         tableSetHolder.style.pointerEvents = 'none';
 
-        document.addEventListener('mousemove', function(ev){
-            document.getElementById('tableSet' + (x+1)).style.transform = 'translateY('+(ev.clientY-115)+'px)';
-            document.getElementById('tableSet' + (x+1)).style.transform += 'translateX('+(ev.clientX-105)+'px)';           
-        },false);
+        tableSetHolder.blockColor = tempAssignedColors[x];
+
+        document.addEventListener('mousemove', moveBlock, false);
     }
 }
 
-function placeElement(xColorsArray){
+function lineCheck(){
+
+    for(let y = 0; y<8; y++){
+
+        if (gameGrid[y].every(value => value === 1)) {
+            console.log('Column ' + y + ' is all 1s');
+            lineClear(y, false);
+        }
+        let isRowFull = true;
+        for(let x = 0; x<8; x++){
+
+            if(gameGrid[x][y] != 1){
+                isRowFull = false;
+                break;
+            }
+        }
+        if(isRowFull){
+            console.log('Row ' + y + ' is all 1s');
+            lineClear(false, y)
+        }
+    }
+}
+
+function lineClear(x, y){
+    if(x){
+        //Column
+        for(let z = 0; z<8; z++){
+            let tempTd = document.getElementById("tdGame_0_" + x + "_" + z);
+            tempTd.style.backgroundColor = "white";
+            gameGrid[x][z] = 0;
+        }
+        updateScore(16);
+    } else{
+        //Row
+        for(let z = 0; z<8; z++){
+            let tempTd = document.getElementById("tdGame_0_" + z + "_" + y);
+            tempTd.style.backgroundColor = "white";
+            gameGrid[z][y] = 0;
+        }
+        updateScore(16);
+    }
+}
+
+function placeElement(){
     const container = document.getElementById('tableGame0');
     container.addEventListener('click', function(event){
         const clickedElement = event.target;
 
+        if (!heldBlockValue) {
+            console.log("No block held for placement.");
+            return;
+        }
 
     // Area emptiness check
-        console.log(heldBlockValue);
+        console.log("Before check:" + heldBlockValue);
 
         let tdGame;
         let counter = 0;
@@ -121,6 +237,7 @@ function placeElement(xColorsArray){
         for(let x=-2;x<3;x++){
             for(let y=-2;y<3;y++){
                 tdGame = document.getElementById(String((clickedElement.id).substring(0, 8) + "_" + (parseInt(clickedElement.id[9])+y) + "_" + (parseInt(clickedElement.id[11])+x)));
+                console.log("Scanning Process: " + String((clickedElement.id).substring(0, 8) + "_" + (parseInt(clickedElement.id[9])+y) + "_" + (parseInt(clickedElement.id[11])+x)));
                 if(heldBlockValue[counter] == 1){
                     if(tdGame && isValid){
                             if(tdGame.style.backgroundColor != "white"){
@@ -139,21 +256,34 @@ function placeElement(xColorsArray){
         }
 
         //If check is sucessfull, fill out the space
+        console.log("After check:" + heldBlockValue);
         if(isValid){
             counter = 0;
             for(let x=-2;x<3;x++){
                 for(let y=-2;y<3;y++){
-                    if(heldBlockValue[counter] == 1){
+                    if(heldBlockValue[counter] == "1"){
                         tdGame = document.getElementById(String((clickedElement.id).substring(0, 8) + "_" + (parseInt(clickedElement.id[9])+y) + "_" + (parseInt(clickedElement.id[11])+x)));
-                        tdGame.style.backgroundColor = tempAssignedColors[tableSetHolder.id[8]-1];
+                        console.log("Placement Process: " + String((clickedElement.id).substring(0, 8) + "_" + (parseInt(clickedElement.id[9])+y) + "_" + (parseInt(clickedElement.id[11])+x)));
+                        console.log ("Placement Process tableSetHolder.id: " + (tableSetHolder.id[8]-1) + ", coressponding tempAssignedColors: " + tableSetHolder.blockColor);
+                        tdGame.style.backgroundColor = tableSetHolder.blockColor;
+
+                        gameGrid[parseInt(clickedElement.id[9])+y][parseInt(clickedElement.id[11])+x] = 1;
                     }
                     counter++;
                 }
             }
+            tableSetHolder.remove();
+            document.removeEventListener('mousemove', moveBlock);
+            let countOnes = (heldBlockValue.match(/1/g) || []).length;
+            updateScore(countOnes);
+            heldBlockValue = 0;
+            tableSetHolder = null;
+            lineCheck();
+        } else {
+            console.log("Block cannot be placed here.");
         }
     })
 }
-
 
 
 function randomBlock(){
@@ -172,3 +302,11 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function updateScore(amount){
+    let score = document.getElementById("score");
+    scoreValue += amount;
+    score.innerHTML = "Score: " + scoreValue;
+}
+
+//Amount of blocks = amount of points
+//One line = cleared line amount x 2
